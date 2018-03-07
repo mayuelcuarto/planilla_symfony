f8fa296a19831508b5233772473ca67a5bdf1a7e
mayuelcuarto <cristhianmayuri@gmail.com>
2018-03-07 08:30:44 -0500

    Implementando interfaz de login
    Diversos cambios en la vista del login, creación del layout base, vista del form y cambio en la ruta principal.

--- src/PlanillaBundle/Controller/DefaultController.php
+++ src/PlanillaBundle/Controller/DefaultController.php
@@ -8,9 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        //return $this->render('@Planilla/Default/index.html.twig');
-        return $this->render('@Planilla/Default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render("@Planilla/User/login.html.twig");
     }
 }
