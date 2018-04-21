e388782400bf10b5d01aa724e3695f30bfd7ef13
mayuelcuarto <cristhianmayuri@gmail.com>
2018-04-20 21:57:10 -0500

    Optimizando Repositorio

--- src/PlanillaBundle/Repository/MetaRepository.php
+++ src/PlanillaBundle/Repository/MetaRepository.php
@@ -8,11 +8,10 @@ class MetaRepository extends EntityRepository {
 
     public function findBySecfuncEstado($secFunc, $estado) {
         return $this->getEntityManager()
-                        ->createQuery(
-                                "SELECT m FROM PlanillaBundle:Meta m 
-                                        WHERE 
-                                        m.estado = :estado OR 
-                                        m.secFunc = :meta")
+                        ->createQuery("SELECT m FROM PlanillaBundle:Meta m 
+                                       WHERE 
+                                       m.estado = :estado OR 
+                                       m.secFunc = :meta")
                         ->setParameter('meta', $secFunc)
                         ->setParameter('estado', $estado)
                         ->getResult();
