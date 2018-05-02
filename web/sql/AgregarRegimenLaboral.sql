206516f1463f302bccd344301113862a3a0576e3
mayuelcuarto <cristhianmayuri@gmail.com>
2018-05-01 19:46:17 -0500

    Añadiendo el campo sueldo_minimo en el procedimiento almacenado

--- web/sql/AgregarRegimenLaboral.sql
+++ web/sql/AgregarRegimenLaboral.sql
@@ -1,8 +1,8 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AgregarRegimenLaboral`(IN id INT, IN nombre VARCHAR(255), IN descripcion VARCHAR(255), IN estado BOOLEAN)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AgregarRegimenLaboral`(IN id INT, IN nombre VARCHAR(255), IN descripcion VARCHAR(255), IN sueldo_minimo DOUBLE, IN estado BOOLEAN)
 BEGIN
 DECLARE varerror INTEGER;
-INSERT INTO regimen_laboral(id, nombre, descripcion, estado)
-VALUES(id, nombre, descripcion, estado);
+INSERT INTO regimen_laboral(id, nombre, descripcion, sueldo_minimo, estado)
+VALUES(id, nombre, descripcion, sueldo_minimo, estado);
 SET varerror = (SELECT @@error_count);
 IF varerror = 0 THEN
 COMMIT;
