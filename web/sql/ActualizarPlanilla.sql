e2b18d67b6282a9f9421e44029d3f2f16e3f4bb7
mayuelcuarto <cristhianmayuri@gmail.com>
2018-05-15 01:19:06 -0500

    Cambios de modelo sql, procedimientos y funciones

--- web/sql/ActualizarPlanilla.sql
+++ web/sql/ActualizarPlanilla.sql
@@ -3,17 +3,19 @@ BEGIN
 DECLARE remAseg DOUBLE;
 DECLARE remNoAseg DOUBLE;
 DECLARE totalEgreso DOUBLE;
+DECLARE patronal DOUBLE;
 
 SET remAseg = (SELECT RemuneracionAsegurable(planilla_id));
 SET remNoAseg = (SELECT RemuneracionNoAsegurable(planilla_id));
 SET totalEgreso = (SELECT TotalEgreso(planilla_id));
+SET patronal = (SELECT CuotaPatronal(planilla_id));
 
 UPDATE planilla p 
 SET 
 p.rem_aseg = remAseg, 
 p.rem_noaseg = remNoAseg, 
 p.total_egreso = totalEgreso,
-p.patronal = 0,
+p.patronal = patronal,
 p.tardanzas = 0,
 p.particulares = 0, 
 p.lsgh= 0,
---
+++ web/sql/CuotaPatronal.sql
@@ -0,0 +1,32 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `CuotaPatronal`(planilla_id INT) RETURNS DOUBLE
+BEGIN
+DECLARE regimenLaboral INT;
+DECLARE sueldoMinimo DOUBLE;
+DECLARE patronal DOUBLE;
+DECLARE remAseg DOUBLE;
+
+SET regimenLaboral = (SELECT ph.regimen_laboral_id FROM planilla p 
+INNER JOIN plaza_historial ph ON ph.id = p.plaza_historial_id
+WHERE p.id = planilla_id AND ph.estado = 1);
+
+SET sueldoMinimo = (SELECT rl.sueldo_minimo FROM regimen_laboral rl WHERE rl.id = regimenLaboral);
+
+SET remAseg = (SELECT RemuneracionAsegurable(planilla_id));
+
+IF regimenLaboral = 1 THEN
+	IF remAseg <= sueldoMinimo THEN
+		SET patronal = sueldoMinimo * 0.09;
+    ELSE
+		SET patronal = remAseg * 0.09;
+    END IF;
+ELSEIF regimenLaboral = 4 THEN
+	IF remAseg >= sueldoMinimo THEN
+		SET patronal = sueldoMinimo * 0.09;
+    ELSE
+		SET patronal = remAseg * 0.09;
+    END IF;
+ELSE 
+	SET patronal = 0;
+END IF;
+RETURN ROUND(patronal, 2);
+END
