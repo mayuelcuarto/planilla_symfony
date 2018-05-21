8a8868b161be96b622413e53bb8dd948f2a0f16f
mayuelcuarto <cristhianmayuri@gmail.com>
2018-05-21 09:01:28 -0500

    Actualización de dump

--- web/sql/dump/planilla2_routines.sql
+++ web/sql/dump/planilla2_routines.sql
@@ -535,9 +535,9 @@ ELSE
 END IF;
 
 SET concepto_monto = tardanzas_aux + particulares_aux + faltas_aux;
+SET concepto_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 82);
 
 IF concepto_monto IS NOT NULL AND concepto_monto > 0 THEN
-	SET concepto_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 82);
 	IF concepto_v IS NULL THEN
 		INSERT INTO planilla_has_concepto(id, monto, fecha_ing, planilla_id, concepto_id, usuario_id)
 		VALUES(0, concepto_monto, NOW(), planilla_id, 82, 1);
@@ -547,14 +547,13 @@ IF concepto_monto IS NOT NULL AND concepto_monto > 0 THEN
 		WHERE id = concepto_v;
 	END IF;
 ELSE 
-	SET concepto_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 82);
 	IF concepto_v IS NOT NULL THEN
 		DELETE FROM planilla_has_concepto WHERE id = concepto_v;
     END IF;
 END IF;
 
+SET lsgh_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 101);
 IF lsgh_aux IS NOT NULL AND lsgh_aux > 0 THEN
-	SET lsgh_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 101);
 	IF lsgh_v IS NULL THEN
 		INSERT INTO planilla_has_concepto(id, monto, fecha_ing, planilla_id, concepto_id, usuario_id)
 		VALUES(0, lsgh_aux, NOW(), planilla_id, 101, 1);
@@ -564,7 +563,6 @@ IF lsgh_aux IS NOT NULL AND lsgh_aux > 0 THEN
 		WHERE id = lsgh_v;
 	END IF;
 ELSE 
-	SET lsgh_v = (SELECT phc.id FROM planilla_has_concepto phc WHERE phc.planilla_id = planilla_id AND phc.concepto_id = 101);
 	IF lsgh_v IS NOT NULL THEN
 		DELETE FROM planilla_has_concepto WHERE id = lsgh_v;
 	END IF;
@@ -959,4 +957,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-17 22:18:55
+-- Dump completed on 2018-05-21  9:00:26
