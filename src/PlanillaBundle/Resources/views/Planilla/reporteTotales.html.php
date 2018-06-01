0a8f93fdccc531977987ee71220d1ab5b3b63f7e
mayuelcuarto <cristhianmayuri@gmail.com>
2018-06-01 14:20:41 -0500

    Modificando detalles en reporteTotales

--- src/PlanillaBundle/Resources/views/Planilla/reporteTotales.html.php
+++ src/PlanillaBundle/Resources/views/Planilla/reporteTotales.html.php
@@ -22,7 +22,7 @@ $hora = date("H:i:s");
         </td>
     </tr>
     <tr><td colspan="7" style="text-align: center;"><b>RESUMEN TOTAL <?php echo $tipoPlanilla->getNombre(); ?> <?php echo $anoEje; ?> - <?php echo $mesEje->getNombre(); ?><br>
-                <?php echo $fuente->getNombre(); ?></b></td></tr>
+<?php echo $fuente->getNombre(); ?></b></td></tr>
     <tr>
         <th style="text-align: center;">Plaza</th>
         <th style="text-align: center;">Apellidos y Nombres</th>
@@ -32,25 +32,25 @@ $hora = date("H:i:s");
         <th style="text-align: center;">Egresos</th>
         <th style="text-align: center;">Neto</th>
     </tr>
-    <?php foreach ($planillas as $p) { ?>
+<?php foreach ($planillas as $p) { ?>
 
         <tr>
             <td><?php echo $p->getPlazaHistorial()->getPlaza()->getTipoPlanilla()->getId() . "-" . $p->getPlazaHistorial()->getPlaza()->getNumPlaza(); ?></td>
             <td><?php echo $p->getPlazaHistorial()->getCodPersonal()->getApellidoPaterno() . " " . $p->getPlazaHistorial()->getCodPersonal()->getApellidoMaterno() . ", " . $p->getPlazaHistorial()->getCodPersonal()->getNombre(); ?></td>
-            <td style="text-align: right;"><?php echo number_format($p->getRemAseg(),2); ?></td>
-            <td style="text-align: right;"><?php echo number_format($p->getRemNoaseg(),2); ?></td>
-            <td style="text-align: right;"><?php echo number_format(($p->getRemAseg() + $p->getRemNoaseg()),2); ?></td>
-            <td style="text-align: right;"><?php echo number_format($p->getTotalEgreso(),2); ?></td>
-            <td style="text-align: right;"><?php echo number_format((($p->getRemAseg() + $p->getRemNoaseg()) - $p->getTotalEgreso()),2); ?></td>
+            <td style="text-align: right;"><?php echo number_format($p->getRemAseg(), 2); ?></td>
+            <td style="text-align: right;"><?php echo number_format($p->getRemNoaseg(), 2); ?></td>
+            <td style="text-align: right;"><?php echo number_format(($p->getRemAseg() + $p->getRemNoaseg()), 2); ?></td>
+            <td style="text-align: right;"><?php echo number_format($p->getTotalEgreso(), 2); ?></td>
+            <td style="text-align: right;"><?php echo number_format((($p->getRemAseg() + $p->getRemNoaseg()) - $p->getTotalEgreso()), 2); ?></td>
         </tr>
-    <?php } ?>
+<?php } ?>
     <tr>
         <td colspan="2" style="text-align: center;"><b>TOTAL</b></td>
-        <td style="text-align: right;"><b><?php echo number_format($sumaRemAseg,2); ?></b></td>
-        <td style="text-align: right;"><b><?php echo number_format($sumaRemNoAseg,2); ?></b></td>
-        <td style="text-align: right;"><b><?php echo number_format(($sumaRemAseg + $sumaRemNoAseg),2); ?></b></td>
-        <td style="text-align: right;"><b><?php echo number_format($sumaTotalEgreso,2); ?></b></td>
-        <td style="text-align: right;"><b><?php echo number_format((($sumaRemAseg + $sumaRemNoAseg) - $sumaTotalEgreso),2); ?></b></td>
+        <td style="text-align: right;"><b><?php echo number_format($sumaRemAseg, 2); ?></b></td>
+        <td style="text-align: right;"><b><?php echo number_format($sumaRemNoAseg, 2); ?></b></td>
+        <td style="text-align: right;"><b><?php echo number_format(($sumaRemAseg + $sumaRemNoAseg), 2); ?></b></td>
+        <td style="text-align: right;"><b><?php echo number_format($sumaTotalEgreso, 2); ?></b></td>
+        <td style="text-align: right;"><b><?php echo number_format((($sumaRemAseg + $sumaRemNoAseg) - $sumaTotalEgreso), 2); ?></b></td>
     </tr>
 </table>
 <?php
