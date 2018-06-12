075c28dcfe2b3099de482a21dc1a00a79bbe7e3f
mayuelcuarto <cristhianmayuri@gmail.com>
2018-06-12 00:08:13 -0500

    Añadiendo función para filtro único de conceptos

--- src/PlanillaBundle/Repository/ConceptoRepository.php
+++ src/PlanillaBundle/Repository/ConceptoRepository.php
@@ -5,10 +5,12 @@ namespace PlanillaBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 use PlanillaBundle\Entity\Mes;
 use PlanillaBundle\Entity\Especifica;
+use PlanillaBundle\Entity\TipoPlanilla;
+use PlanillaBundle\Entity\FuenteFinanc;
 use PlanillaBundle\Entity\TipoConcepto;
 
 class ConceptoRepository extends EntityRepository {
-    
+
     public function findDistinctMetasEspTC($anoEje, Mes $mesEje, Especifica $especifica, TipoConcepto $tipoConcepto) {
         return $this->getEntityManager()
                         ->createQuery("SELECT DISTINCT(c.id) AS id, c.concepto AS concepto, c.abreviatura AS abreviatura
@@ -27,4 +29,28 @@ class ConceptoRepository extends EntityRepository {
                         ->setParameter('tipoConcepto', $tipoConcepto)
                         ->getResult();
     }
+
+    public function findArrayDistinctAnoMesTPFuente($anoEje, Mes $mesEje, TipoPlanilla $tipoPlanilla, FuenteFinanc $fuente, TipoConcepto $tipoConcepto) {
+        return $this->getEntityManager()
+                        ->createQuery("SELECT DISTINCT(c.id) AS id, c.concepto AS nombre
+                                       FROM PlanillaBundle:PlanillaHasConcepto phc
+                                       INNER JOIN phc.planilla p
+                                       INNER JOIN phc.concepto c
+                                       INNER JOIN p.plazaHistorial ph
+                                       INNER JOIN ph.plaza pl
+                                       WHERE 
+                                       p.anoEje = :anoEje AND 
+                                       p.mesEje = :mesEje AND
+                                       pl.tipoPlanilla = :tipoPlanilla AND
+                                       p.fuente = :fuente AND 
+                                       c.tipoConcepto = :tipoConcepto
+                                       ORDER BY c.id")
+                        ->setParameter('anoEje', $anoEje)
+                        ->setParameter('mesEje', $mesEje)
+                        ->setParameter('tipoPlanilla', $tipoPlanilla)
+                        ->setParameter('fuente', $fuente)
+                        ->setParameter('tipoConcepto', $tipoConcepto)
+                        ->getArrayResult();
+    }
+
 }
