7e7e233c85817d6baaea1f4463cd69eba0fceed5
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-22 23:22:02 -0500

    Reemplazando sql

--- web/sql/planilla_has_concepto_AFTER_INSERT.sql
+++ web/sql/planilla_has_concepto_AFTER_INSERT.sql
@@ -1,5 +1,4 @@
-CREATE DEFINER = CURRENT_USER TRIGGER `planilla2`.`planilla_has_concepto_AFTER_INSERT` AFTER INSERT ON `planilla_has_concepto` FOR EACH ROW
-BEGIN
+CREATE DEFINER=`root`@`localhost` TRIGGER `planilla_has_concepto_AFTER_INSERT` AFTER INSERT ON `planilla_has_concepto` FOR EACH ROW BEGIN
 CALL ActualizarPlanilla(NEW.planilla_id);
 INSERT INTO planilla_concepto_auditoria(id, planilla_concepto_id, fecha, detalle)
 VALUES(0, NEW.id, NOW(),
